3879f63b59f5fe7fa31d260b2cc07949f7ef63ee
snndiaa <snndiaaa@gmail.com>
2025-09-16 19:35:00 +0300

    added changes

--- classwork_16.09/MyString.cpp
+++ classwork_16.09/MyString.cpp
@@ -9,6 +9,7 @@ MyString::MyString()
     str = new char[length] {};
 }
 
+
 MyString::MyString(int size)
 {
     length = size;
@@ -18,7 +19,7 @@ MyString::MyString(int size)
 MyString::MyString(const char* st)
 {
     length = strlen(st);
-    str = new char[length+1];
+    str = new char[length + 1];
     strcpy_s(str, length + 1, st);
 }
 
@@ -54,12 +55,12 @@ MyString::MyString(const MyString& other)
 
 bool MyString::MyStrStr(const char* str)
 {
-	const char* s = strstr(this->str, str);
+    const char* s = strstr(this->str, str);
     if (s != nullptr)
     {
         return true;
     }
-	return false;
+    return false;
 }
 
 int MyString::MyChr(char c)
@@ -67,18 +68,59 @@ int MyString::MyChr(char c)
     for (int i = 0; i < length; ++i)
     {
         if (str[i] == c) return i;
-    }   
+    }
     return -1;
 }
 
 int MyString::MyStrLen()
 {
-    return static_cast<int>(strlen(str));
+    return strlen(str);
 }
 
-MyString::MyString(const MyString& other)
+void MyString::MyStrCat(MyString& b)
 {
-    length = other.length;
-    str = new char[length + 1];
-    strcpy_s(str, length + 1, other.str);
-} 
+    int len_a = strlen(this->str);
+    int len_b = strlen(b.str);
+    char* temp = new char[len_a + len_b + 1];
+
+    strcpy_s(temp, len_a + len_b + 1, this->str);
+    strcat_s(temp, len_a + len_b + 1, b.str);
+
+    delete[] this->str;
+    this->str = temp;
+    this->length = len_a + len_b;
+}
+
+void MyString::MyDelChr(char c)
+{
+    int keep = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (str[i] != c) keep++;
+    }
+
+
+    char* temp = new char[keep + 1];
+    int j = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (str[i] != c)
+        {
+            temp[j++] = str[i];
+        }
+    }
+
+    temp[j] = '\0';
+
+    delete[] str;
+    str = temp;
+    length = keep;
+}
+
+int MyString::MyStrCmp(MyString& b)
+{
+    int cmp = strcmp(this->str, b.str);
+    if (cmp < 0) return -1;
+    if (cmp > 0) return 1;
+    return 0;
+}
--- classwork_16.09/main.cpp
+++ classwork_16.09/main.cpp
@@ -4,6 +4,49 @@ using namespace std;
 
 int main()
 {
-	MyString obj1("Hello World");
-	obj1.print();
+    MyString s1("Arina");
+    MyString s2("Vanya");
+
+    s1.print();
+    s2.print();
+
+    MyString s3 = s1;
+    s3.print();
+
+    if (s1.MyStrStr("ri")) 
+    {
+        cout << "Substring found in Arina\n";
+    }
+    else
+    {
+        cout << "Substring not found in Arina\n";
+    }
+
+    cout << "Index of 'n' in Vanya: " << s2.MyChr('n') << endl;
+
+    cout << "Length of Arina: " << s1.MyStrLen() << endl;
+
+    s1.MyStrCat(s2);
+    cout << "After concatenation: ";
+    s1.print();
+
+    s1.MyDelChr('a');
+    cout << "After deleting 'a': ";
+    s1.print();
+
+    int cmp = s2.MyStrCmp(s3);
+    if (cmp == 0)
+    {
+        cout << "Vanya == Arina\n";
+    }
+    else if (cmp < 0)
+    {
+        cout << "Vanya < Arina\n";
+    }
+    else
+    {
+        cout << "Vanya > Arina\n";
+    }
+
+    return 0;
 }
